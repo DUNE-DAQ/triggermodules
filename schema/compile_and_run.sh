3769dab182132f92d98f2ba9b665e733aea63317
Lukas Arnold <lukas.arnold@nevis.columbia.edu>
2021-02-09 13:08:50 -0500

    minor changes

--- schema/compile_and_run.sh
+++ schema/compile_and_run.sh
@@ -1,7 +1,7 @@
 APPNAME=simplest_trigger_app
-APPNAME=1toNTP_trigger_app
 APPNAME=full_trigger_app
 APPNAME=csv_trigger_app
+APPNAME=1toNTP_trigger_app
 APPNAME=full_csv_trigger_app
 cd ../../..
 moo compile sourcecode/DAQDuneTriggers/schema/"$APPNAME".jsonnet > sourcecode/DAQDuneTriggers/schema/"$APPNAME"_compiled.json
